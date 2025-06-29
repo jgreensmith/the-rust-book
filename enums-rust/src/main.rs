15619f1736d07f9d91c0f5c9f8535a17b287d451
James <jgreensmith13@gmail.com>
2025-06-29 14:21:47 +0100

    Downloading client state

--- enums-rust/src/main.rs
+++ enums-rust/src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+
+struct DldInProg {
+    size: f32
+}
+impl DldInProg {
+    fn percentage_done(&self, done: f32) -> i32 {
+        (100.0 / self.size * done).ceil() as i32
+    }
+}
+
+#[derive(Debug)]
+
+enum ClientState {
+    Downloading(DldInProg),
+    Downloaded,
+    Cloud
+
+}
+
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
         None => None,
@@ -11,6 +31,14 @@ fn main() {
     let six = plus_one(five);
     let none = plus_one(None);
     let ten = plus_one(Some(10));
+
+    let state = ClientState::Downloading(DldInProg { size: 60.0 });
+    let downloaded: f32 = 30.0;
+    if let ClientState::Downloading(prog) = state {
+        assert_eq!(prog.percentage_done(downloaded), 50);
+        println!("{}% Downloaded", prog.percentage_done(downloaded));
+    }
+
 
     assert_eq!(six, Some(6));
     assert_eq!(none, None);
