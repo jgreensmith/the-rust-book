9bd67e6a14a9b5f94673d5300784b233c78f95f9
James <jgreensmith13@gmail.com>
2025-08-26 11:00:31 +0100

    asdf

--- boxes/src/main.rs
+++ boxes/src/main.rs
@@ -1,10 +1,55 @@
 use crate::List::{Cons, Nil};
+use std::any::type_name;
+use std::ops::Deref;
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
+//tuple struct with generic type T
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+//custom drop message
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    let mybox = MyBox::new(4);
+    assert_eq!(4, *mybox);
+
+        let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
 }
