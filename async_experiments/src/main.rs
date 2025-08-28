d5078aa11b4b59529c57c9a55668625769f3a1d7
James <jgreensmith13@gmail.com>
2025-08-28 11:43:06 +0100

    pin

--- async_experiments/src/main.rs
+++ async_experiments/src/main.rs
@@ -1,5 +1,5 @@
 use std::time::Duration;
-use std::pin::Pin;
+use std::pin::{Pin, pin};
 
 fn main() {
     trpl::run(async {
@@ -21,27 +21,27 @@ fn main() {
 
         let tx1 = tx.clone();
             
-        let fut1 = async move {
+        let fut1 = pin!(async move {
             for i in 1..10 {
                 tx1.send(format!("Task A: section {i}")).unwrap();
                 trpl::sleep(Duration::from_millis(50)).await;
             }
-        };
+        });
 
         let tx2 = tx.clone();
         
-        let fut2 = async move {
+        let fut2 = pin!(async move {
             for i in 1..5 {
                 tx2.send(format!("Task B: section {i}")).unwrap();
-                trpl::sleep(Duration::from_millis(50)).await;
+                trpl::sleep(Duration::from_millis(55)).await;
             }
-        };
+        });
             
             
             // rx/ tx futures 
 
         
-        let tx_fut = async move {
+        let tx_fut = pin!(async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -51,20 +51,20 @@ fn main() {
                 
                 for val in vals {
                     tx.send(val).unwrap();
-                    trpl::sleep(Duration::from_millis(50)).await;
+                    trpl::sleep(Duration::from_millis(60)).await;
                 }
-            };
+            });
             
-            let rx_fut = async {
+            let rx_fut = pin!(async {
                 
                 // while not None
                 while let Some(value) = rx.recv().await {
                     println!("received '{value}'");
                 }
-            };
+            });
                 
-            let fut_vec: Vec<Pin<Box<dyn Future<Output = ()>>>> =
-                vec![Box::pin(fut1), Box::pin(fut2), Box::pin(tx_fut), Box::pin(rx_fut)];
+            let fut_vec: Vec<Pin<&mut dyn Future<Output = ()>>> =
+                vec![fut1, fut2, tx_fut , rx_fut ];
 
             trpl::join_all(fut_vec).await;
 
