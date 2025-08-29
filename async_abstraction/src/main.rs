4404387149f35adc969109b1140b796a77e09bcb
James <jgreensmith13@gmail.com>
2025-08-29 10:27:00 +0100

    asdf

---
+++ async_abstraction/src/main.rs
@@ -0,0 +1,29 @@
+use std::time::Duration;
+use trpl::Either;
+
+async fn timeout<F: Future>(
+    future_to_try: F,
+    max_time: Duration,
+) -> Result<F::Output, Duration> {
+    
+    match trpl::race(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }}
+
+fn main() {
+    trpl::run(async {
+        let slow = async {
+            trpl::sleep(Duration::from_secs(5)).await;
+            "I finished!"
+        };
+
+        match timeout(slow, Duration::from_secs(2)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs())
+            }
+        }
+
+    });
+}
